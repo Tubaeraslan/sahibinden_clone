2ee6da564cb7682dda27205ec0143fcc6ee78057
Tuba Eraslan <tubaeraslan@Mac.home>
2025-09-30 17:27:46 +0300

    First commit with MVC

---
+++ src/main/java/starter/SahibindenCloneApplication.java
@@ -0,0 +1,13 @@
+package starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SahibindenCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SahibindenCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tubaeraslan/sahibinden_clone/SahibindenCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tubaeraslan.sahibinden_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SahibindenCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
