57c93665739253fc43fc8456551f4647a7c010ad
tubaeraslan <tubaeraslan193@gmail.com>
2025-10-02 22:51:43 +0300

    added dto for brand

---
+++ src/main/java/dto/requestDto/BrandRequestDto.java
@@ -0,0 +1,10 @@
+package dto.requestDto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BrandRequestDto {
+    private String name;
+}
---
+++ src/main/java/dto/responseDto/BrandResponseDto.java
@@ -0,0 +1,11 @@
+package dto.responseDto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BrandResponseDto {
+    private Integer id;
+    private String name;
+}
