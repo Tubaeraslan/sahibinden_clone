2ec53066c4fea07a26b268419eb1f3d1fdf052c1
tubaeraslan <tubaeraslan193@gmail.com>
2025-10-03 15:47:04 +0300

    Added car entity and dto. But need to car brand class and database connection.

---
+++ src/main/java/entities/Car.java
@@ -0,0 +1,46 @@
+package entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "cars")
+public class Car {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "year")
+    private Integer year;
+
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "km")
+    private Integer km;
+
+    @Column(name = "color")
+    private String color;
+
+    //Brand class ı ile merge le table lar arası ilişki kur
+    //private Brand brand;
+
+    public Car(){};
+
+    public Car(Integer id, String model, Integer year, double price, Integer km, String color) {
+        this.id = id;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.km = km;
+        this.color = color;
+    }
+}
