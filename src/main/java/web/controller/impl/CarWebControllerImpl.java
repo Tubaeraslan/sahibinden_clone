8c304f0b3ef59ec1cbf86fff8305db31fc21e060
tubaeraslan <tubaeraslan193@gmail.com>
2025-10-17 14:02:35 +0300

    add_car page is implemented

--- src/main/java/web/controller/impl/CarWebControllerImpl.java
+++ src/main/java/web/controller/impl/CarWebControllerImpl.java
@@ -1,13 +1,17 @@
 package web.controller.impl;
 
 import controller.ICarController;
+import dto.requestDto.CarRequestDto;
+import entities.Brand;
 import entities.Car;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import repository.BrandRepository;
+import repository.CarRepository;
+import service.IBrandService;
+import service.ICarService;
 import web.controller.ICarWebController;
 import web.service.ICarWebService;
 import web.service.impl.CarWebServiceImpl;
@@ -17,7 +21,20 @@ import java.util.List;
 
 @Controller
 public class CarWebControllerImpl implements ICarWebController {
+
     private final ICarWebService carWebService;
+
+    @Autowired
+    private ICarService carService;
+
+    @Autowired
+    private IBrandService brandService;
+
+    @Autowired
+    private BrandRepository brandRepository;
+
+    @Autowired
+    private CarRepository carRepository;
 
     @Autowired
     public CarWebControllerImpl(CarWebServiceImpl carWebService) {
@@ -68,6 +85,35 @@ public class CarWebControllerImpl implements ICarWebController {
         // Thymeleaf’e gönder
         model.addAttribute("cars", cars);
         return "cars"; // templates/cars.html
+    }
+
+    @PostMapping("/cars/add")
+    public String addCarPost(@ModelAttribute("carRequest") CarRequestDto carRequest) {
+        carService.addCar(carRequest);  // Veritabanına kaydet
+        return "redirect:/cars";        // İşlem sonrası liste sayfasına yönlendir
+    }
+
+
+//    public void addCar(CarRequestDto carRequest) {
+//        Car car = new Car();
+//        car.setModel(carRequest.getModel());
+//        car.setYear(carRequest.getYear());
+//        car.setPrice(carRequest.getPrice());
+//        car.setKm(carRequest.getKm());
+//        car.setColor(carRequest.getColor());
+//
+//        Brand brand = brandRepository.findById(carRequest.getBrandId())
+//                .orElseThrow(() -> new RuntimeException("Brand not found"));
+//        car.setBrand(brand);
+//
+//        carRepository.save(car);
+//    }
+
+    @GetMapping("/cars/add")
+    public String showAddCarForm(Model model) {
+        model.addAttribute("carRequest", new CarRequestDto());
+        model.addAttribute("brands", brandService.getAllBrands(0,20)); // Tüm markaları çek
+        return "add_car";
     }
 
 }
